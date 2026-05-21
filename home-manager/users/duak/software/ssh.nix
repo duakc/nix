800d01aab4faa88bf3382af38b371a407fec51b9
duakc <young@qeee.net>
2026-05-21 15:32:15 +0800

    feat: update flake

--- home-manager/users/duak/software/ssh.nix
+++ home-manager/users/duak/software/ssh.nix
@@ -12,19 +12,30 @@ in
       "/etc/ssh/ssh_config.d/*.conf"
       "${homeDirectory}/${sshHome}/config.d/*.conf"
     ];
-    matchBlocks = {
+    settings = {
       "*" = {
-        identityFile = "${homeDirectory}/${sshHome}/id_ed25519";
-        sendEnv = [ "TERM" ];
-        setEnv = {
+        ForwardAgent = lib.mkDefault false;
+        AddKeysToAgent = lib.mkDefault "no";
+        ServerAliveInterval = lib.mkDefault 0;
+        ServerAliveCountMax = lib.mkDefault 3;
+        HashKnownHosts = lib.mkDefault false;
+        UserKnownHostsFile = lib.mkDefault "~/.ssh/known_hosts";
+        ControlMaster = lib.mkDefault "no";
+        ControlPath = lib.mkDefault "~/.ssh/master-%r@%n:%p";
+        ControlPersist = lib.mkDefault "no";
+
+        IdentityFile = "${homeDirectory}/${sshHome}/id_ed25519";
+        SendEnv = [ "TERM" ];
+        Compression = true;
+        SetEnv = {
           TERM = "xterm-256color";
         };
       };
       "github.com" = {
-        hostname = "ssh.github.com";
-        user = "git";
+        Hostname = "ssh.github.com";
+        User = "git";
         # use 443 to bypass 22 block in some servers
-        port = 443;
+        Port = 443;
       };
     };
   };
